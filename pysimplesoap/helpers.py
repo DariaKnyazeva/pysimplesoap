ba1b92ef0665e64467e299780b6516f69f8156d5
Daria Knyazeva <dashaa2@gmail.com>
2018-07-27 14:05:13 +0600

    Update helpers.py

--- pysimplesoap/helpers.py
+++ pysimplesoap/helpers.py
@@ -84,7 +84,7 @@ def fetch(url, http, cache=False, force_download=False, wsdl_basedir='', headers
             if not os.path.isdir(cache):
                 os.makedirs(cache)
             f = open(filename, 'w')
-            f.write(xml)
+            f.write(xml.decode('utf-8'))
             f.close()
     return xml
 
