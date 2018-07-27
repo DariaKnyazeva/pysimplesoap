f53323882789f28586f45d972445d7f7d7ac58d7
Daria Knyazeva <dashaa2@gmail.com>
2018-07-27 13:02:41 +0600

    encode xml cache

--- pysimplesoap/helpers.py
+++ pysimplesoap/helpers.py
@@ -84,6 +84,7 @@ def fetch(url, http, cache=False, force_download=False, wsdl_basedir='', headers
             if not os.path.isdir(cache):
                 os.makedirs(cache)
             f = open(filename, 'w')
+            xml = xml.encode('utf-8')
             f.write(xml)
             f.close()
     return xml
